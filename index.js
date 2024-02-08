e1e6da3907789f1ca4cc429d3068beb81a9f8cc9
danruk <149634927+danruk@users.noreply.github.com>
2024-02-07 22:28:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+// let person={
+//     name:"Danny Rukundo",
+//     age:33
+// };
+// person.name="Mr.Right";
+// //namee=1;
+// console.log(person.name);   
+
+// let colors =['blue','green'];
+// console.log(colors[0]);
+
+// function greet(name){
+//     console.log('Hello '+name);
+// }
+// greet('Ronaldo');  
+// let Usercolor="red";
+// let defaultcolor="blue";
+// let currentColor=Usercolor || defaultcolor;
+// console.log(currentColor   );
+
+//conditions
+// let hour=10;
+// if(hour=10 ){
+//     console.log("youre correct");
+// }
+
+//loops 
+// for(let i=0;i<5;i++){
+//        console.log("wow",i);
+
+// }
+
+//for in
+// let person={
+//     name:"danruk",
+//     age:30};
+    
+//     for(let key in person)
+//     console.log(key, person[key]);
+
+//max
+// let number= max(4,3);
+// console.log(number)
+// function max(a,b){
+//     // if(a>b){
+//     //     return a;}
+//     //     else 
+//     //     return b;
+//     return (a>b)? a:b;
+}
+
